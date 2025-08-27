e697c4a6540ef5937b2406a8fbc736cfedf25c25
zxoou-media <foridulhouk@gmail.com>
2025-08-27 14:29:38 +0600

    Update app.py

--- cinelist/app.py
+++ cinelist/app.py
@@ -23,4 +23,4 @@ def images(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)0
+    app.run(debug=True, host='0.0.0.0', port=port)
